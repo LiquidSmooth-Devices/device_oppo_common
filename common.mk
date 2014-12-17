c1aa582aa7f91b2f1e4abb7743d8bbec1c1aa87a
Darescu Ionut <ionut.darescu@gmail.com>
2014-12-17 12:11:24 -0500

    Don't build DeviceHandler just yet...
    Change-Id: Id5661e4e41b0677167098d8d98fa1439c30ccb13

--- common.mk
+++ common.mk
@@ -18,8 +18,8 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # DevicHandler
-PRODUCT_PACKAGES += \
-	DeviceHandler
+#PRODUCT_PACKAGES += \
+#	DeviceHandler
 
 # Keyhandler
 PRODUCT_PACKAGES += \
